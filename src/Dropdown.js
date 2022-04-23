9a7b374196441fc233096ad6451d4bfbd292de2f
Somdatta <sd2187@gmail.com>
2022-04-23 09:34:01 +0530

    update Dropdown.js

--- src/Dropdown.js
+++ src/Dropdown.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import onClickOutside from 'react-onclickoutside';
-import './scss/App.scss';
+import './App.scss';
 
 function Dropdown({ title, items, multiSelect = false }) {
   const [open, setOpen] = useState(false);
